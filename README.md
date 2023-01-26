2ed4c9da40ee22a91790c728babb5a9ceddc12ce
Kirill Chernyshov <kchernys@cisco.com>
2023-01-26 12:45:52 +0100

    Prepare release

--- README.md
+++ README.md
@@ -2,23 +2,35 @@
 
 [RFC 8785 JSON Canonicalization Scheme (JCS)](https://www.rfc-editor.org/rfc/rfc8785) extension for [metosin/jsonista](https://github.com/metosin/jsonista).
 
-# TLDR;
+## deps.edn
 
-## Include as a git-dependency (maven release will be cut off later) in `deps.edn`
-
-``` edn
-{:deps {io.github.DotFox/jsonista.jcs {:git/sha "99faeaa5da5eee98d682a4601e6e5f9b7adea44f"}}}
+```clojure
+dev.dotfox/jsonista.jcs {:mvn/version "1.0.8"}
 ```
 
-## Prep java sources
+or as a git dependency
 
-``` shell
+```clojure
+io.github.DotFox/jsonista.jcs {:git/sha "99faeaa5da5eee98d682a4601e6e5f9b7adea44f"}
+```
+
+and prep java sources
+
+```shell
 clj -X:deps prep
 ```
 
+## project.clj
+
+```clojure
+[dev.dotfox/jsonista.jcs "1.0.8"]
+```
+
+# TLDR;
+
 ## Create custom `object-mapper`
 
-``` clojure
+```clojure
 (require '[jsonista.core :as json] '[jsonista.jcs :as jcs])
 
 ;; jcs/object-mapper expects the same arguments as json/object-mapper
--- build.clj
+++ build.clj
@@ -1,18 +1,72 @@
 (ns build
   (:refer-clojure :exclude [compile])
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
 
 (def lib 'dev.dotfox/jsonista.jcs)
-(def version (format "1.0.%s" (b/git-count-revs nil)))
+(defn- the-version
+  ([patch] (the-version patch false))
+  ([patch snapshot?] (str "1.0." patch (when snapshot? "-SNAPSHOT"))))
+(def version (the-version (b/git-count-revs nil)))
+(def snapshot (the-version (b/git-count-revs nil) true))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn clean [_]
-  (b/delete {:path "target"}))
+(defn clean [opts]
+  (println "Cleaning target directory...")
+  (b/delete {:path "target"})
+  opts)
 
-(defn compile [_]
+(defn compile [opts]
+  (println "Compiling java sources...")
   (b/javac {:src-dirs ["src/java"]
             :class-dir class-dir
             :basis basis
-            :javac-opts ["-source" "8" "-target" "8"]}))
+            :javac-opts ["-source" "8" "-target" "8"]})
+  opts)
+
+(defn- jar-opts [opts]
+  (let [version (if (:snapshot opts) snapshot version)]
+    (assoc opts
+           :lib lib
+           :version version
+           :jar-file jar-file
+           :scm {:tag (str "v" version)}
+           :basis (b/create-basis {})
+           :class-dir class-dir
+           :target "target"
+           :src-dirs ["src/clj" "src/java" "target/classes"]
+           :src-pom "pom.xml")))
+
+(defn jar [opts]
+  (let [opts (jar-opts opts)]
+    (println "Writing pom.xml...")
+    (b/write-pom opts)
+    (println "Copying source...")
+    (b/copy-dir {:src-dirs ["src/clj" "src/java"] :target-dir class-dir})
+    (println "Building JAR...")
+    (b/jar opts)
+    opts))
+
+(defn deploy [opts]
+  (let [{:keys [jar-file installer] :as opts} (jar-opts opts)]
+    (dd/deploy {:installer installer
+                :artifact (b/resolve-path jar-file)
+                :pom-file (b/pom-path (select-keys opts [:lib :class-dir]))})))
+
+(defn deploy-remote [opts]
+  (-> opts
+      (clean)
+      (compile)
+      (jar)
+      (assoc :installer :remote)
+      (deploy)))
+
+(defn deploy-local [opts]
+  (-> opts
+      (clean)
+      (compile)
+      (jar)
+      (assoc :installer :local)
+      (deploy)))
